aff88c6c0dc7893ff71908e8598c76f15f6c22cb
jharnakhatun2 <jharnakhatun203@gmail.com>
2023-11-18 20:34:01 +0600

    added problem solving about sort()

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-// Write a function that can print any random number between any two given numbers (including them)
+// 1. Write a function that can print any random number between any two given numbers (including them)
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min; 
 }
 console.log(getRandomNumber(1, 6));
+
+
+// 2. How can you order roll numbers of students in your class sequentially?
+const students = ["Jharna", "Khatun", "Abir", "Monir", "Shakib"];
+students.sort();
+console.log(students);
