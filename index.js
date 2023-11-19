eb4b5e6fd95ddc92c52240d593c9d286adef0332
jharnakhatun2 <jharnakhatun203@gmail.com>
2023-11-19 10:16:00 +0600

    added total vowel number solution

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // 1. Write a function that can print any random number between any two given numbers (including them)
 
 function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min; 
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 console.log(getRandomNumber(1, 6));
-
 
 // 2. How can you order names of students in your class sequentially?
 const students = ["Jharna", "Khatun", "Abir", "Monir", "Shakib"];
@@ -13,16 +12,29 @@ console.log(students);
 
 // 3. How can you order roll numbers of students in your class sequentially?
 const rollNo = [3, 5, 1, 6, 2, 30];
-rollNo.sort((a,b) => a-b);
+rollNo.sort((a, b) => a - b);
 console.log(rollNo);
-
 
 //4. Write a function that can tell us whether an year is a leap year or not.
 function isLeapYear(year) {
-    if((year % 400 === 0) || ((year % 4 === 0) && (year % 100 !== 0))){
-        console.log(`${year} is leap year`);
-    }else{
-        console.log(`${year} is not leap year`);
-    }
+  if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+    console.log(`${year} is leap year`);
+  } else {
+    console.log(`${year} is not leap year`);
+  }
 }
 isLeapYear(2024);
+
+// 5.  How would you determine the number of vowels in a sentence?
+const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+function totalVowelNumber(sentence) {
+    let count = 0;
+    const letters = Array.from(sentence);
+    letters.forEach(function(value) {
+        if(vowels.includes(value)){
+            count++;
+        }
+    })
+    return count;
+}
+console.log(totalVowelNumber("Bangladesh is a Golden Country among world"))
