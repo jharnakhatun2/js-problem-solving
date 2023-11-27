3ab88d4b647031d302016387d5d18dc20ee55e47
jharnakhatun2 <jharnakhatun203@gmail.com>
2023-11-27 19:04:38 +0600

    added problem solve two

--- array.js
+++ array.js
@@ -18,3 +18,42 @@ console.log(is_array("I love you"));
 // console.log(array_Clone([1, 2, [4, 0]]));
 // [1, 2, 4, 0]
 // [1, 2, [4, 0]]
+
+function array_Clone(value) {
+    return value.slice();
+}
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
+
+
+// 3. Write a JavaScript function to get the first element of an array. Passing the parameter 'n' will return the first 'n' elements of the array.
+// Test Data :
+// console.log(first([7, 9, 0, -2]));
+// console.log(first([],3));
+// console.log(first([7, 9, 0, -2],3));
+// console.log(first([7, 9, 0, -2],6));
+// console.log(first([7, 9, 0, -2],-3));
+// Expected Output :
+// 7
+// []
+// [7, 9, 0]
+// [7, 9, 0, -2]
+// []
+
+function first(array, n) {
+    if(array == null){
+        return void 0;
+    }
+    if(n == null) {
+        return array[0];
+    }
+    if(n < 0) {
+        return [];
+    }
+    return array.slice(0, n);
+}
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
