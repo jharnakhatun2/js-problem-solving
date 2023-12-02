1cf660a9c8e83a85ecf18945ca0abb9773582ae0
jharnakhatun2 <jharnakhatun203@gmail.com>
2023-12-02 23:21:31 +0600

    added 5th problem solve with array

--- array.js
+++ array.js
@@ -81,3 +81,16 @@ function last(array, n) {
 console.log(last([7, 9, 0, -2]));
 console.log(last([7, 9, 0, -2],3));
 console.log(last([7, 9, 0, -2],6));
+
+
+// 5. Write a simple JavaScript program to join all elements of the following array into a string.
+//Sample array : myColor = ["Red", "Green", "White", "Black"];
+// Expected Output:
+// "Red,Green,White,Black"
+// "Red,Green,White,Black"
+// "Red+Green+White+Black"
+
+const myColor = ["Red", "Green", "White", "Black"];
+console.log(myColor.toString());
+console.log(myColor.join());
+console.log(myColor.join('+'));
---
+++ test.js
@@ -0,0 +1,4 @@
+const array = [1,2,3,4,5,6,7]
+let result = array[6];
+
+console.log(result);
