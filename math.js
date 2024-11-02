b0c1d22b43544ce6da7fcac000726f79c27e4624
Jharna Khatun <jharnakhatun203@gmail.com>
2024-11-02 19:07:04 +0600

    add math methods

---
+++ math.js
@@ -0,0 +1,13 @@
+console.log(Math.min(24,95,1,45,23, -8));
+console.log(Math.max(24,95,1,45,23, -8));
+console.log(Math.PI);
+console.log(Math.abs(-99));
+console.log(Math.round(2.89));
+console.log(Math.floor(2.89));
+console.log(Math.ceil(2.89));
+// between : 0 - 1
+console.log(Math.random());
+// between : 0 - 10
+console.log(Math.random() * 10);
+// between : 0 - 10 integer number
+console.log(Math.round(Math.random() * 10));
