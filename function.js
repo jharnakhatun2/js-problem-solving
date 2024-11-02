1eb86e3fd064bb88b9f8c9ceb592f3c9e897b78c
Jharna Khatun <jharnakhatun203@gmail.com>
2024-11-02 11:26:20 +0600

    added problem solve task 4

--- function.js
+++ function.js
@@ -45,8 +45,23 @@ function make_avg(array){
 
 const result2 = make_avg(arr, average);
 console.log(result2);
+
+
 // Task-4
 // Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
+
+function count_zero(binaryNum) {
+    let count = 0
+    for(let num of binaryNum){
+        if(num === '0'){
+            count++;
+        }
+    }
+
+return count;
+}
+const resA = count_zero("16546865314530000002514896412323000");
+console.log(resA);
 
 // Task-5
 // Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
