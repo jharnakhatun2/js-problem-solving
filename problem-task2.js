df0d203e78db81cffc7537f27deea51333c1577e
Jharna Khatun <jharnakhatun203@gmail.com>
2024-11-03 10:39:52 +0600

    problem solution - tallest and smallest function logic

--- problem-task2.js
+++ problem-task2.js
@@ -30,3 +30,32 @@ console.log(maxThee);
 // Get maximum between unlimited
 const max = Math.max(85,92,14,75,105);
 console.log(max);
+
+// Who is the tallest? Find the max number in an array
+const height = [56,81,97,49,62,67,83,59,73];
+function tallest(array){
+    let max = array[0];
+for(let item of array){
+    if(item > max){
+        max = item;
+    }
+}
+return max;
+}
+const tallestHeight = tallest(height);
+console.log(tallestHeight);
+
+
+// Who is the smallest (vertically privilege )? Find the max number in an array
+
+function smallest(array){
+    let min = array[0];
+for(let item of array){
+    if(item < min){
+        min = item;
+    }
+}
+return min;
+}
+const smallestHeight = smallest(height);
+console.log(smallestHeight);
