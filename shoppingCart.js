5a1b83a41d13ea4d8aaada7204f6664afd86a835
Jharna Khatun <jharnakhatun203@gmail.com>
2024-11-03 11:58:51 +0600

    problem solve - calculate total cost in product cart with quantity

--- shoppingCart.js
+++ shoppingCart.js
@@ -15,3 +15,22 @@ return totalPrice;
 }
 const cartPrice = calculateShoppingCart(products);
 console.log("Total product price : " , cartPrice);
+
+
+// Calculate the total cost of the products with quantity in a shopping cart
+const productWithQuantity = [
+    {name: 'Shampoo', price: 300, quantity: 3 },
+    {name: 'Chiruni', price: 100, quantity: 1 },
+    {name: 'Shirt', price: 700, quantity: 2 },
+    {name: 'Pant', price: 1200, quantity: 2}
+]
+function calculateShoppingCart(products){
+    let totalPrice = 0;
+for(const product of products){
+    totalPrice += product.price * product.quantity;
+}
+return totalPrice;
+}
+const totalCartPrice = calculateShoppingCart(productWithQuantity);
+console.log("Total product price with quantity: " , totalCartPrice);
+
