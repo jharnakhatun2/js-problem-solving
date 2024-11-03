271af74ced06bae876adde55bfdd244ac06221d1
Jharna Khatun <jharnakhatun203@gmail.com>
2024-11-03 08:57:37 +0600

    Count vowels

--- problem-task.js
+++ problem-task.js
@@ -2,12 +2,11 @@
 // Write a function to convert temperature from Celsius to Fahrenheit.
 //°F = °C × (9/5) + 32
 function celsiusToFahrenheit(celsius) {
-    const fahrenheit = celsius * (9/5) +32;
-    return `${Math.round(fahrenheit)}°F`;
+  const fahrenheit = celsius * (9 / 5) + 32;
+  return `${Math.round(fahrenheit)}°F`;
 }
 const result = celsiusToFahrenheit(25);
 console.log(result);
-
 
 // Task-2:
 // You are given an array of numbers. Count how many times the a number is repeated in the array.
@@ -19,19 +18,33 @@ console.log(result);
 // numbers = [5,6,11,12,98, 5]
 // find: 25
 // output: 0
-const numbers = [5,6,11,12,98, 5];
+const numbers = [5, 6, 11, 12, 98, 5];
 
 function findNumber(array, element) {
-    return array.reduce((count, item) => item === element ? count + 1 : count, 0);
+  return array.reduce(
+    (count, item) => (item === element ? count + 1 : count),
+    0
+  );
 }
 
 const findResult = findNumber(numbers, 25);
-console.log(findResult); ;
-
+console.log(findResult);
 
 // Task-3:
 // Write a function to count the number of vowels in a string.
-
+// a, e, i, o, u
+function countVowels(str) {
+  const smallStr = str.toLowerCase();
+  let count = 0;
+  for (let item of str) {
+    if('aeiou'.includes(item)){
+        count ++
+    }
+  }
+  return count;
+}
+const resultForCountVowels = countVowels(" your code is working fine");
+console.log(resultForCountVowels);
 // Task-4:
 // Write a function to find the longest word in a given string.
 
