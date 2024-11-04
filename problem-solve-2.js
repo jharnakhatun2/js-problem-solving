f1a4add6938d32bf4b6f9ea92ab8ad24ed845e6d
Jharna Khatun <jharnakhatun203@gmail.com>
2024-11-04 09:42:22 +0600

    problem solved - average price of phone from array of object

--- problem-solve-2.js
+++ problem-solve-2.js
@@ -3,37 +3,36 @@ Find the lowest number in the array below.
 const heights2 = [167, 190, 120, 165, 137];*/
 
 const heights2 = [167, 190, 120, 165, 137];
-function countLowestNumber(array){
-    if(Array.isArray(array)=== false){
-        return 'Enter an array as input'
+function countLowestNumber(array) {
+  if (Array.isArray(array) === false) {
+    return "Enter an array as input";
+  }
+  let count = array[0];
+  for (let item of array) {
+    if (item < count) {
+      count = item;
     }
-let count = array[0];
-for(let item of array){
-if(item < count){
-    count = item ;
-}
-}
-return count;
+  }
+  return count;
 }
 const lowestNumber = countLowestNumber(23);
 console.log(lowestNumber);
 
-
 /*Task -2:
 Find the friend with the smallest name.
 const friends = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];*/
-const friends = ['rahim', 'robin', 'rafi', 'ron', 'de' ,'rashed'];
-function smallestName(array){
-    if(Array.isArray(array)=== false){
-        return " enter array of string"
+const friends = ["rahim", "robin", "rafi", "ron", "de", "rashed"];
+function smallestName(array) {
+  if (Array.isArray(array) === false) {
+    return " enter array of string";
+  }
+  let count = array[0];
+  for (const name of array) {
+    if (name.length < count.length) {
+      count = name;
     }
-let count = array[0];
-for(const name of array){
-    if(name.length < count.length){
-        count = name
-    }
-}
-return count;
+  }
+  return count;
 }
 // const smallNameFriend = smallestName('jharna');
 const smallNameFriend = smallestName(friends);
@@ -46,21 +45,28 @@ Your task is to calculate the total budget required to buy electronics:
     tablet = 15000 tk
     mobile = 20000 tk
 Write a JavaScript function named calculateElectronicsBudget that takes in the number of laptop, tablets, and mobile and returns the total money required.*/
-function calculateElectronicsBudget(laptopQuantity, tabletsQuantity, mobileQuantity){
-    if(typeof laptopQuantity !== 'number' || typeof tabletsQuantity !== 'number' || typeof mobileQuantity !== 'number'){
-        return "Please enter valid quantity as number."
-    }
-    const laptop = 35000 ;
-    const tablet = 15000 ;
-    const mobile = 20000 ;
-    const laptopPrice = laptop * laptopQuantity ;
-    const tabletPrice = tablet * tabletsQuantity ;
-    const mobilePrice = mobile * mobileQuantity;
-    const totalBudget = laptopPrice + tabletPrice + mobilePrice;
-    return totalBudget;
-
+function calculateElectronicsBudget(
+  laptopQuantity,
+  tabletsQuantity,
+  mobileQuantity
+) {
+  if (
+    typeof laptopQuantity !== "number" ||
+    typeof tabletsQuantity !== "number" ||
+    typeof mobileQuantity !== "number"
+  ) {
+    return "Please enter valid quantity as number.";
+  }
+  const laptop = 35000;
+  const tablet = 15000;
+  const mobile = 20000;
+  const laptopPrice = laptop * laptopQuantity;
+  const tabletPrice = tablet * tabletsQuantity;
+  const mobilePrice = mobile * mobileQuantity;
+  const totalBudget = laptopPrice + tabletPrice + mobilePrice;
+  return totalBudget;
 }
-const result = calculateElectronicsBudget(2,4,'amar');
+const result = calculateElectronicsBudget(2, 4, "amar");
 console.log(result);
 
 /*Task-4:
@@ -77,6 +83,29 @@ Input
         { model: "PhoneE", brand: "Iphone", price: 105000 },
         { model: "PhoneF", brand: "HTC", price: 48000 },
     ];*/
+const phones = [
+  { model: "PhoneA", brand: "Iphone", price: 95000 },
+  { model: "PhoneB", brand: "Samsung", price: 40000 },
+  { model: "PhoneC", brand: "Oppo", price: 26000 },
+  { model: "PhoneD", brand: "Nokia", price: 35000 },
+  { model: "PhoneE", brand: "Iphone", price: 105000 },
+  { model: "PhoneF", brand: "HTC", price: 48000 },
+];
+function findAveragePhonePrice(array) {
+  if (!Array.isArray(array)) {
+    return "Please enter valid array of object with price";
+  }
+  let sum = 0;
+  for (const obj of array) {
+    sum += obj.price;
+  }
+  const averagePriceOfPhone = sum / array.length;
+  return Math.round(averagePriceOfPhone);
+}
+const averagePhonePrice = findAveragePhonePrice(phones);
+// const averagePhonePrice = findAveragePhonePrice("phones");
+console.log(averagePhonePrice);
+
 /*Task -5: (Hard)
 For each employee their current salary is calculated by multiplying yearly increment with experience then adding the result to the starting salary. Now calculate is the total salary has to be provided by the company in a month.
 
