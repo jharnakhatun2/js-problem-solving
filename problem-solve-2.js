fe7198ca90bf8f9d825b1fd3d953a63a30e0c559
Jharna Khatun <jharnakhatun203@gmail.com>
2024-11-04 10:18:06 +0600

    problem solve - total salary has to be provided by the company in a month

--- problem-solve-2.js
+++ problem-solve-2.js
@@ -115,3 +115,22 @@ For each employee their current salary is calculated by multiplying yearly incre
             { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
             { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
         ];*/
+        const employees = [
+            { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+            { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+            { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+            { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+        ]
+        function salaryCalculator(array){
+            if (!Array.isArray(array)) {
+                return "Please enter valid array of object";
+              }
+            let givenTotalSalary = 0 ;
+            for(const employee of array){
+                const currentSalary  =   (employee.increment * employee.experience) + employee.starting;
+                givenTotalSalary += currentSalary ;
+            }
+            return givenTotalSalary;
+        }
+       const totalSalary = salaryCalculator(employees);
+        console.log(totalSalary);
